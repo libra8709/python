cbb8ecf1a548712064ded1915923086fe6ce40ff
Libra <libra.lyu@gmail.com>
2016-04-25 08:56:47 +0800

    20160425

--- chapter7/athletemodel.py
+++ chapter7/athletemodel.py
@@ -1,4 +1,17 @@
 # -*- coding:utf-8 -*-
 
 import pickle
-from athlete
+from AthleteList import AthleteList
+
+def get_coach_data(filename):
+    
+    
+def put_to_store(files_list):
+    all_athletes={}
+    for flist in files_list:
+        try:
+            with open(flist) as data:
+                data_txt=data.readline()
+                datalist=data_txt.strip.split(",")
+                all_athletes=AthleteList(datalist.pop(0),datalist.pop(0),datalist)
+        except:
